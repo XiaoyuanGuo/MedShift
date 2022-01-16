284e82786ed3059d242e8eb4a7f158f8d8dac2ef
Sophie <xiaoyuanguo.ucas@gmail.com>
2022-01-15 19:28:26 -0500

    Update dataset.py

--- MURA/Classification/dataset.py
+++ MURA/Classification/dataset.py
@@ -24,7 +24,7 @@ data_transforms = {
     ]),
 }
 
-class Emory_Allclass_Dataset(Dataset):
+class MURA_Allclass_Dataset(Dataset):
     def __init__(self, data, mode='train'):
         self.data = data
         self.mode = mode   
